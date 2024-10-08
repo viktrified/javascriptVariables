9a13d858878a980e5e57858ab0771537858ea8c7
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-10-08 13:32:58 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let name = "Viktrified";
-let age = "21";
+// let name = "Viktrified";
+// let age = "21";
+// let userName = name.substr(0, 4) + age;
 
-let userName = name.substr(0, 4) + age;
+// console.log(userName);
+
+let name = "Viktrified";
+let age = 21;
+let gender = "male";
+let nationality = "Nigeria";
+
+let userName =
+  "My name is " +
+  name +
+  ", I'm " +
+  age +
+  " years old " +
+  "and I'm a " +
+  gender +
+  " from " +
+  nationality;
 
 console.log(userName);
